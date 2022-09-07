a846798d6b7a0b5f9707bd385c09abce4846d845
pythagoras26 <barconadia38@gmail.com>
2022-09-06 21:50:44 -0300

    Clase 5

---
+++ Clase5/app.js
@@ -0,0 +1,32 @@
+alert(`hola`);
+
+//CASE SENSITIVE Nombre =/= nombre 
+
+/*Tipado debil --> el valor de la variable cambia por el ultimo valor expuesto 
+let nombre = Bruce
+nombre =708 cuando cambio el valor
+*/
+
+
+//Variables
+var nombre2 ='Juan'; 
+let apellido='Perez'; //Tipo de string
+let num1 =345 //number
+let boolean= true
+const comi9dafavorita= 'Pizza';
+
+let number=10;
+let number2='10'
+let resultado =number*number2 //Lo multiplica 
+let resultado2 = number - number2//Lo resta, vale la concadenacion solo con la suma
+let resultado3= number + number2
+let nombreNuevo= prompt("decime tu nombre");
+
+console.log(apellido)
+
+document.write(nombre2);
+document.write(resultado+" "+resultado2+" "+resultado3)
+
+document.write('hola ' +nombreNuevo)
+
+
